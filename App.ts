1228744650dccdca0f5d8c74ce0ef86ca8dcfd91
pritiprajapati314 <pritiprajapati314@gmail.com>
2022-05-02 22:37:23 +0530

    "pop up implementation"

--- App.ts
+++ App.ts
@@ -18,13 +18,14 @@ let buttonFindPath = document.getElementById('buttonFindPath');
 let speed = document.getElementById("speedSlider") as HTMLInputElement;
 
 
+
 speed.oninput = () =>{
     console.log("no");
     board.painter.speedSelection(100/Math.floor(parseInt(speed.value)));
 }
 
+
 board.algorithmID = 0;
-    console.log("idea");
 buttonBFS.onclick = () => {
     board.algorithmID = 1;
     buttonFindPath.innerHTML = "Breadth First Search";
@@ -84,4 +85,30 @@ buttonClearScreen.onclick = () =>{
 
 butttonAddWeight.onclick = () =>{
     board.addWeights();
+}
+
+
+
+//pop up board 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close");
+
+// When the user clicks the button, open the modal 
+modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
